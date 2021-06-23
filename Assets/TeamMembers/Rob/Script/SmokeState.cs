3dda5838ee8222576074084c0c0d1b908e3afa45
Rob <1021549@student.sae.edu.au>
2021-06-23 17:08:02 +1000

    Smoke and water changing colour

--- Assets/TeamMembers/Rob/Script/SmokeState.cs
+++ Assets/TeamMembers/Rob/Script/SmokeState.cs
@@ -8,14 +8,18 @@ namespace Rob
 {
     public class SmokeState : StateBase
     {
+        private Renderer rend;
+        
         public override void Enter()
         {
             base.Enter();
+            rend = GetComponent<Renderer>();
         }
 
         public override void Execute()
         {
             base.Execute();
+            rend.material.SetColor("Color", Color.gray);
         }
 
         public override void Exit()
--- Assets/TeamMembers/Rob/Script/WaterState.cs
+++ Assets/TeamMembers/Rob/Script/WaterState.cs
@@ -11,14 +11,19 @@ namespace Rob
 
     public class WaterState : StateBase
     {
+        private Renderer rend;
+        
         public override void Enter()
         {
             base.Enter();
+            Debug.Log("Entered");
+            rend = GetComponent<Renderer>(); //getting the renderer of the tile
         }
 
         public override void Execute()
         {
             base.Execute();
+            rend.material.SetColor("Color", Color.blue);
         }
 
         public override void Exit()
