65ea795a6e95fe9a7460cde04768fe972d9df805
Riley McGowan <1017093@student.sae.edu.au>
2021-06-29 15:23:04 +1000

    Sneaky wall push

---
+++ Assets/TeamMembers/Riley/ExampleCode/WallSpawnCode.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawnCode : MonoBehaviour
+{
+    void Start()
+    {
+        CheckSides();
+    }
+
+    void CheckSides()
+    {
+        Ray ray = new Ray();
+    }
+}
