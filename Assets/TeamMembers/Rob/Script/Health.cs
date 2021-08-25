1a8318480595a30f6bc2b0eec82bc0109fffeda5
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-08-25 12:19:29 +1000

    Healbar to the players

--- Assets/TeamMembers/Rob/Script/Health.cs
+++ Assets/TeamMembers/Rob/Script/Health.cs
@@ -9,20 +9,19 @@ using UnityEngine;
 
 public class Health : NetworkBehaviour
 {
-    [SyncVar]
-    public float currentHealth;
+    [SyncVar] public float currentHealth;
     public float maxHealth;
     public HealthBar healthBar;
 
 
-    private void Start()
+    public override void OnStartServer()
     {
+        base.OnStartServer();
         currentHealth = maxHealth;
         healthBar.RpcSetMaxHealth(maxHealth);
     }
-
-
-     public void OnEnable()
+    
+    public void OnEnable()
      {
          FindObjectOfType<EventManager>().DeathEvent += DeathEvent;
          FindObjectOfType<EventManager>().OnDamageEvent += DamageEventTaken;
@@ -38,21 +37,23 @@ public class Health : NetworkBehaviour
 
      public void DamageEventTaken(float damageAmount)
     {
-        currentHealth -= damageAmount;
-        healthBar.RpcSetHealth(currentHealth);
-        if (currentHealth <= 0)
+        if (isServer)
         {
-            currentHealth = 0;
-            healthBar.RpcSetHealth(currentHealth);
-            Debug.Log("Dead");
-        }
-        Debug.Log("Damaged current health is now " + currentHealth);
-        if (currentHealth <= 0)
-        {
-            FindObjectOfType<EventManager>().CallDeathEvent();
+            currentHealth -= damageAmount;
             
+            healthBar.RpcSetHealth(currentHealth);
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                healthBar.RpcSetHealth(currentHealth);
+                Debug.Log("Dead");
+            }
+            Debug.Log("Damaged current health is now " + currentHealth);
+            if (currentHealth <= 0)
+            {
+                FindObjectOfType<EventManager>().CallDeathEvent();
+            }
         }
-        
     }
 
     public void DeathEvent()
